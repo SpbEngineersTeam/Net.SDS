20356c9865a254b560121b23930fc700ff38bb0e
Alex_K <axxel93@gmail.com>
2018-02-04 22:42:52 +0300

    Добавил новые свойства в класс записи в реестре.

--- src/Net.SDS.Registry/Net.SDS.Registry.DataAccess.Abstractions/Entities/ServiceInstanceEntity.cs
+++ src/Net.SDS.Registry/Net.SDS.Registry.DataAccess.Abstractions/Entities/ServiceInstanceEntity.cs
@@ -3,8 +3,51 @@ namespace Net.SDS.ServiceDiscovery.Abstractions.Entities
 {
     public class ServiceInstanceEntity
     {
+        /// <summary>
+        /// Возвращает / устанавливает Guid сервиса.
+        /// </summary>
         public Guid ServiceId { get; set; }
+
+        /// <summary>
+        /// Возвращает / устанавливает версию сервиса.
+        /// </summary>
         public string Version { get; set; }
+
+        /// <summary>
+        /// Возвращает / устанавливает URI сервиса.
+        /// </summary>
         public string Uri { get; set; }//TODO: string -> System.Uri?
+
+        /// <summary>
+        /// Возвращает / устанавливает описание сервиса.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Возвращает / устанавливает дату регистрации сервиса в реестре.
+        /// </summary>
+        public DateTimeOffset RegisteredDate { get; set; }
+
+        /// <summary>
+        /// Возвращает / устанавливает дату обновления состояния сервиса в реестре.
+        /// </summary>
+        public DateTimeOffset UpdatedDate { get; set; }
+
+        /// <summary>
+        /// Возвращает / устанавливает признак работоспособности сервиса в данный момент.
+        /// </summary>
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// Возвращает / устанавливает допустимую продолжительность времени неработоспособности сервиса. 
+        /// По достижении данного времени сервис будет помечен как неактивный.
+        /// </summary>
+        public TimeSpan AllowableDowntime { get; set; }
+
+        /// <summary>
+        /// Возвращает / устанавливает максимально возможную продолжительность времени неработоспособности сервиса. 
+        /// По достижении данного времени сервис будет удалён из реестра.
+        /// </summary>
+        public TimeSpan MaxDowntime { get; set; }
     }
 }
